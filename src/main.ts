1646f3eafeb16c57fccc94183216a3e342553e5a
Mohammad MohammadAlian <mralian.dev@gmail.com>
2022-09-04 19:01:45 +0430

    add prisma and fix configuration bug

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 import type { AppConfig } from './config/configuration';
+import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService<AppConfig>);
+  const prismaService = app.get(PrismaService);
+
+  prismaService.enableShutdownHook(app);
 
   // Null-safety is handled in Service
   const port = configService.get('port')!;
