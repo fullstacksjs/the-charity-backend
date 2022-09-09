01a3c238394159b81ecc05e70980df8f7edc2517
Alireza Safaierad <frontendmonster@gmail.com>
2022-09-09 19:40:39 +0400

    [Hotfix]: Update ESLint Config (#8)
    Update @fullstacksjs/eslint-config to v7.0.2.
    Add a custom naming convention.
    Removed unused ESLint disable comments.

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,56 @@ module.exports = {
   root: true,
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/no-extraneous-class': 'off',
+    '@typescript-eslint/naming-convention': [
+      'warn',
+      {
+        selector: 'default',
+        format: ['camelCase'],
+      },
+      {
+        selector: 'function',
+        format: ['camelCase', 'PascalCase'],
+      },
+      // variables, CONSTANTS, ReactComponents
+      {
+        selector: 'variable',
+        format: ['camelCase', 'UPPER_CASE', 'PascalCase'],
+      },
+      {
+        selector: 'parameter',
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allowSingleOrDouble',
+      },
+      {
+        selector: 'memberLike',
+        format: null,
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['static'],
+        format: ['camelCase', 'PascalCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'memberLike',
+        modifiers: ['private'],
+        format: ['camelCase'],
+        leadingUnderscore: 'allow',
+      },
+      {
+        selector: 'typeLike',
+        format: ['PascalCase'],
+      },
+      {
+        selector: 'enumMember',
+        format: ['PascalCase'],
+      },
+      // Disallow I prefix for interfaces
+      {
+        selector: 'interface',
+        format: ['PascalCase'],
+        custom: { regex: '^I[A-Z]', match: false },
+      },
+    ],
   },
 };
