20174727e7a3af5c0777fbfec5d2d1f889220016
Hossein Pourdavar <h.pourdavar@gmail.com>
2022-09-25 18:49:41 +0330

    Revert "[FSK-85]: Extract The GraphQL Module (#25)"
    This reverts commit d8e0a9bc6bf7f9780e43a40cbac8341630dae426.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,31 @@
+import type { ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloDriver } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 
 import { AdminModule } from './admin/admin.module';
+import type { EnvironmentVariables } from './configuration/EnvironmentVariables';
 import validationSchema from './configuration/validation';
 import { DependentModule } from './dependent/dependent.module';
 import { FamilyModule } from './family/family.module';
-import { GraphQLConfigModule } from './graphql-config/graphql-config.module';
 import { HouseholderModule } from './householder/householder.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProjectModule } from './project/project.module';
+import { IBAN } from './shared/scalars/iban.scalar';
+import { Money } from './shared/scalars/money.scalar';
+
+const mocks = {
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  DateTime: () => {
+    return new Date();
+  },
+  // eslint-disable-next-line @typescript-eslint/naming-convention
+  String: () => {
+    return 'string-mock';
+  },
+};
 
 @Module({
   imports: [
@@ -17,7 +34,26 @@ import { ProjectModule } from './project/project.module';
       isGlobal: true,
       validationSchema,
     }),
-    GraphQLConfigModule,
+    GraphQLModule.forRootAsync<ApolloDriverConfig>({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      useFactory: (configService: ConfigService<EnvironmentVariables>) => ({
+        playground: false,
+        debug: true,
+        mocks,
+        autoSchemaFile: true,
+        resolvers: { Money, IBAN },
+        plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        introspection: configService.get('INTROSPECTION_ENABLED'),
+        cors: {
+          // TODO: origin should be moved into configModule - not sure regex is valid!
+          // https://www.debuggex.com/r/XTrC-yG2Yeq2_sAm
+          origin: /https?:\/\/[a-z0-9\-_]+-fullstacks\.vercel\.app/,
+          credentials: true,
+        },
+      }),
+    }),
     ProjectModule,
     FamilyModule,
     HouseholderModule,
--- src/graphql-config/scalerMocks.ts
+++
@@ -1,5 +0,0 @@
-export const scalerMocks = {
-  DateTime: () => new Date(),
-  Money: () => '0',
-  IBAN: () => 'IR13123123123131',
-};
