590cfc8bbbd69370c9248b9a3bcdb9324b83da55
Mohammad Mohammadalian <46217391+mhmda-83@users.noreply.github.com>
2022-10-07 16:45:23 +0330

    [FSK-75]: Add CreateFamily Mutation (#33)
    add createFamily mutation and related tests.

--- src/family/family.service.spec.ts
+++ src/family/family.service.spec.ts
@@ -1,9 +1,12 @@
+import { faker } from '@faker-js/faker';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
 import type { MockProxy } from 'jest-mock-extended';
 import { mockDeep } from 'jest-mock-extended';
+import slug from 'slug';
 
 import { PrismaService } from '../prisma/prisma.service';
+import type { CreateFamilyInput } from './dto/input/create-family-input.dto';
 import { FamilyService } from './family.service';
 import { familyStub } from './stubs';
 
@@ -53,6 +56,44 @@ describe('FamilyService', () => {
     it('should return unique family', async () => {
       jest.spyOn(prisma.family, 'findUnique').mockResolvedValue(familyStub);
       const family = await service.findById('familyId');
+
+      expect(family).toEqual(familyStub);
+    });
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(service.create).toBeDefined();
+    });
+
+    it('should called with correct argument', async () => {
+      const data: CreateFamilyInput = { name: faker.name.fullName() };
+      const method = jest
+        .spyOn(prisma.family, 'create')
+        .mockResolvedValue(familyStub);
+      await service.create(data);
+
+      expect(method).toHaveBeenCalledWith({
+        data: { ...data, slug: slug(data.name) },
+      });
+    });
+
+    it('should throw unrecognized error', async () => {
+      const errorMessage = 'Wrong Input';
+
+      jest
+        .spyOn(prisma.family, 'create')
+        .mockRejectedValue(new Error(errorMessage));
+
+      await expect(
+        service.create({ name: faker.name.fullName() }),
+      ).rejects.toThrow(errorMessage);
+    });
+
+    it('should return created family', async () => {
+      jest.spyOn(prisma.family, 'create').mockResolvedValue(familyStub);
+      const family = await service.create({ name: familyStub.name });
 
       expect(family).toEqual(familyStub);
     });
--- src/family/family.service.ts
+++ src/family/family.service.ts
@@ -1,10 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import type { Family } from '@prisma/client';
+import { nanoid } from 'nanoid';
+import slug from 'slug';
 
 import { PrismaService } from '../prisma/prisma.service';
+import type { CreateFamilyInput } from './dto/input/create-family-input.dto';
 
 @Injectable()
 export class FamilyService {
+  private readonly logger = new Logger(FamilyService.name);
+
   constructor(private readonly prisma: PrismaService) {}
 
   findAll() {
@@ -17,5 +22,29 @@ export class FamilyService {
         id,
       },
     });
+  }
+
+  async create(data: CreateFamilyInput): Promise<Family> {
+    const familySlug = slug(data.name);
+    const extendedData = {
+      ...data,
+      slug: familySlug,
+    };
+
+    const isFamilyExists = await this.prisma.family.count({
+      where: { slug: familySlug },
+    });
+
+    if (isFamilyExists) {
+      extendedData.slug = `${familySlug}-${nanoid(5)}`;
+    }
+
+    try {
+      const family = await this.prisma.family.create({ data: extendedData });
+      return family;
+    } catch (error) {
+      this.logger.error({ errorOnCreateFamily: error });
+      throw error;
+    }
   }
 }
