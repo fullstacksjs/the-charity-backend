66437d00f7e3f9cc023e3c8971c34ccbca3c4d9a
Mohammad Foroutan <50955936+MohammadForoutan@users.noreply.github.com>
2022-10-07 13:49:13 +0330

    [FSK-55]: Create Project Implement Resolvers (#31)
    - Project entity match with DB schema
    - Resolvers implementation
    - Do a mutation by API call to create a project
    - Created project must be stored in the DB
    - Have unit tests
    - To have API tests

--- src/project/project.resolver.spec.ts
+++ src/project/project.resolver.spec.ts
@@ -1,21 +1,146 @@
-import type { TestingModule } from '@nestjs/testing';
+import type { INestApplication } from '@nestjs/common';
+import { GraphQLSchemaHost } from '@nestjs/graphql';
 import { Test } from '@nestjs/testing';
+import type { PrismaClient } from '@prisma/client';
+import { ApolloServer, gql } from 'apollo-server-express';
 
-import { ProjectResolver } from './project.resolver';
-import { ProjectService } from './project.service';
+import { AppModule } from '../app.module';
+import { PrismaService } from '../prisma/prisma.service';
+import type { CreateProjectInput } from './dto/input';
+import { ProjectStatus } from './entities/project-status.enum';
 
-describe('ProjectResolver', () => {
-  let resolver: ProjectResolver;
+// eslint-disable-next-line max-lines-per-function
+describe('Project Query', () => {
+  let app: INestApplication;
+  let apolloServer: ApolloServer;
+  let prisma: PrismaClient;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectResolver, ProjectService],
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
     }).compile();
 
-    resolver = module.get<ProjectResolver>(ProjectResolver);
+    app = moduleRef.createNestApplication();
+    await app.init();
+    const { schema } = app.get(GraphQLSchemaHost);
+    apolloServer = new ApolloServer({ schema });
+    prisma = app.get(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(resolver).toBeDefined();
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('Create', () => {
+    it('Should create a project without optional fields', async () => {
+      const input: CreateProjectInput = { name: 'new name' };
+      const query = gql`
+        mutation createProject($input: CreateProjectInput!) {
+          createProject(input: $input) {
+            id
+            name
+            description
+            status
+            updated_at
+            created_at
+          }
+        }
+      `;
+
+      const result = await apolloServer.executeOperation({
+        query,
+        variables: { input },
+      });
+
+      const project = await prisma.project.findFirst({
+        where: { id: result.data?.['createProject'].id },
+      });
+
+      expect(project).toBeTruthy();
+      expect(project).toMatchObject(input);
+
+      expect(result.errors).toBeFalsy();
+      expect(result.data).toBeTruthy();
+      expect(result.data?.['createProject']).toBeTruthy();
+      expect(result.data?.['createProject']).toMatchObject({
+        ...input,
+        status: ProjectStatus.PLANNING,
+        description: null,
+      });
+
+      await prisma.project.delete({
+        where: { id: result.data?.['createProject'].id },
+      });
+    });
+
+    it('Should create a project with optional fields', async () => {
+      const input: CreateProjectInput = {
+        name: 'new name',
+        description: 'SOME-DESC',
+      };
+      const query = gql`
+        mutation createProject($input: CreateProjectInput!) {
+          createProject(input: $input) {
+            id
+            name
+            description
+            status
+            updated_at
+            created_at
+          }
+        }
+      `;
+
+      const result = await apolloServer.executeOperation({
+        query,
+        variables: { input },
+      });
+
+      const project = await prisma.project.findUnique({
+        where: { id: result.data?.['createProject'].id },
+      });
+
+      expect(project).toBeTruthy();
+      expect(project).toMatchObject(input);
+
+      expect(result.errors).toBeFalsy();
+      expect(result.data).toBeTruthy();
+      expect(result.data?.['createProject']).toBeTruthy();
+      expect(result.data?.['createProject']).toMatchObject({
+        ...input,
+        status: ProjectStatus.PLANNING,
+      });
+
+      await prisma.project.delete({
+        where: { id: result.data?.['createProject'].id },
+      });
+    });
+
+    it('Should throw error for required fields', async () => {
+      const input = { description: 'SOME-DESC2' };
+      const query = gql`
+        mutation createProject($input: CreateProjectInput!) {
+          createProject(input: $input) {
+            id
+            name
+            description
+            status
+            updated_at
+            created_at
+          }
+        }
+      `;
+
+      const result = await apolloServer.executeOperation({
+        query,
+        variables: { input },
+      });
+
+      const project = await prisma.project.findFirst();
+
+      expect(project).toBeNull();
+      expect(result.errors).toBeTruthy();
+      expect(result.data).toBeUndefined();
+    });
   });
 });
--- src/project/project.service.spec.ts
+++ src/project/project.service.spec.ts
@@ -1,20 +1,82 @@
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import { ProjectStatus } from '@prisma/client';
+import type { MockProxy } from 'jest-mock-extended';
+import { mockDeep } from 'jest-mock-extended';
 
+import { PrismaService } from '../prisma/prisma.service';
+import type { CreateProjectInput } from './dto/input';
 import { ProjectService } from './project.service';
+import { projectStub } from './stubs';
 
 describe('ProjectService', () => {
   let service: ProjectService;
+  let prisma: MockProxy<PrismaService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProjectService],
+      providers: [
+        ProjectService,
+        {
+          provide: PrismaService,
+          useFactory: () => mockDeep<PrismaService>(),
+        },
+      ],
     }).compile();
 
     service = module.get<ProjectService>(ProjectService);
+    prisma = module.get(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('create', () => {
+    it('should be defined', () => {
+      // eslint-disable-next-line @typescript-eslint/unbound-method
+      expect(service.create).toBeDefined();
+    });
+
+    it('should called with', async () => {
+      const data: CreateProjectInput = { name: 'HELLO' };
+      const method = jest
+        .spyOn(prisma.project, 'create')
+        .mockResolvedValue(projectStub);
+      await service.create(data);
+
+      expect(method).toHaveBeenCalledWith({ data });
+    });
+
+    it('should create a new project without optional fields', async () => {
+      const data: CreateProjectInput = { name: 'test' };
+      jest
+        .spyOn(prisma.project, 'create')
+        .mockResolvedValue({ ...projectStub, ...data });
+      const project = await service.create(data);
+
+      expect(project).toMatchObject({
+        ...data,
+        description: null,
+        status: ProjectStatus.PLANNING,
+      });
+    });
+
+    // eslint-disable-next-line jest/no-focused-tests
+    it('should create a new project with optional fields', async () => {
+      const data: CreateProjectInput = {
+        name: 'test',
+        description: 'SOME DESC',
+      };
+      jest
+        .spyOn(prisma.project, 'create')
+        .mockResolvedValue({ ...projectStub, ...data });
+      const project = await service.create(data);
+
+      expect(project).toMatchObject({
+        ...data,
+        status: ProjectStatus.PLANNING,
+      });
+    });
   });
 });
