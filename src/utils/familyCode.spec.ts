c3424cc3dd9632d6ad63018945ac2a3fe20fa024
Mohammad MohammadAlian <mralian.dev@gmail.com>
2022-10-16 19:27:36 +0330

    add test cases for not expected cases

--- src/utils/familyCode.spec.ts
+++ src/utils/familyCode.spec.ts
@@ -8,11 +8,25 @@ describe('FamilyCode util', () => {
     expect(convertCodeNumberToFamilyCode(99)).toBe('F00099');
     expect(convertCodeNumberToFamilyCode(1)).toBe('F00001');
     expect(convertCodeNumberToFamilyCode(99999)).toBe('F99999');
+    expect(() => {
+      convertCodeNumberToFamilyCode(NaN);
+    }).toThrow();
+    expect(() => {
+      convertCodeNumberToFamilyCode(123456);
+    }).toThrow();
+    expect(() => {
+      convertCodeNumberToFamilyCode(-1);
+    }).toThrow();
   });
 
   it('should extract the code number from family code', () => {
     expect(extractCodeNumberFromFamilyCode('F00001')).toBe(1);
     expect(extractCodeNumberFromFamilyCode('F00099')).toBe(99);
     expect(extractCodeNumberFromFamilyCode('F99999')).toBe(99999);
+    expect(() => {
+      extractCodeNumberFromFamilyCode(
+        'SomeRandomStringWhichDoesNotHaveAnyNumber',
+      );
+    }).toThrow();
   });
 });
