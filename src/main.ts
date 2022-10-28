10ee910be7255cedde4a3d3b1385f7dddc03deec
Alireza Safaierad <frontendmonster@gmail.com>
2022-10-28 23:32:01 +0400

    fix heroku issue

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,16 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import type { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from './app.module';
 import type { Config, ServerConfig } from './configuration';
 import { PrismaService } from './prisma/prisma.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // https://stackoverflow.com/questions/65767024/express-session-not-working-in-production-deployment
+  app.enable('trust proxy');
 
   const configService = app.get(ConfigService<Config, true>);
   const prismaService = app.get(PrismaService);
